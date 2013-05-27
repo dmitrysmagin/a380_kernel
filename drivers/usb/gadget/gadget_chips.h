618f39854cf3815e91f1312ee586f4362f4acee4
Dmitry Smagin <exmortis@yandex.ru>
2013-05-27 23:18:54 +0400

    usb gadget: JZ4750D: Add dirty usb gadget drivers
    usb gadget: JZ4750D: Remove UDC_USE_LB_CACHE option and a hack from file_storage.c

--- drivers/usb/gadget/gadget_chips.h
+++ drivers/usb/gadget/gadget_chips.h
@@ -15,6 +15,12 @@
 #ifndef __GADGET_CHIPS_H
 #define __GADGET_CHIPS_H
 
+#ifdef CONFIG_USB_GADGET_JZ4750D
+#define	gadget_is_jz4750d(g)	!strcmp("jz4740_udc", (g)->name)
+#else
+#define	gadget_is_jz4750d(g)	0
+#endif
+
 #ifdef CONFIG_USB_GADGET_NET2280
 #define	gadget_is_net2280(g)	!strcmp("net2280", (g)->name)
 #else
@@ -200,6 +206,9 @@ static inline int usb_gadget_controller_number(struct usb_gadget *gadget)
 		return 0x25;
 	else if (gadget_is_s3c_hsotg(gadget))
 		return 0x26;
+	else if (gadget_is_jz4750d(gadget))
+		return 0x27;
+
 	return -ENOENT;
 }
 
