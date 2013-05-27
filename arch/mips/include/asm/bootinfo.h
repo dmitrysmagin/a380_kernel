190b0e76022f4056f0e18ff4dfe38e88396e46f2
Dmitry Smagin <exmortis@yandex.ru>
2013-05-27 20:13:29 +0400

    MIPS: Add JZRISC arch definitions

--- arch/mips/include/asm/bootinfo.h
+++ arch/mips/include/asm/bootinfo.h
@@ -57,6 +57,15 @@
 #define	MACH_MIKROTIK_RB532	0	/* Mikrotik RouterBoard 532 	*/
 #define MACH_MIKROTIK_RB532A	1	/* Mikrotik RouterBoard 532A 	*/
 
+/*
+ * Valid machtype for group INGENIC
+ */
+#define  MACH_INGENIC_JZ4730	0	/* JZ4730 SOC		*/
+#define  MACH_INGENIC_JZ4740	1	/* JZ4740 SOC		*/
+#define  MACH_INGENIC_JZ4750	2	/* JZ4750 SOC		*/
+#define  MACH_INGENIC_JZ4750D	3	/* JZ4750D SOC		*/
+#define  MACH_INGENIC_JZ4750L	4	/* JZ4750L SOC		*/
+
 #define CL_SIZE			COMMAND_LINE_SIZE
 
 extern char *system_type;
