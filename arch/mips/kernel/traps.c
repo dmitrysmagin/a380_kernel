a94bfe769d7def97c1ac4c91d4e397b4512ef89d
Exmortis <exmortis@yandex.ru>
2013-05-28 13:29:15 +0400

    Silence stupid GCC errors on panic() call with static char foo[] argument

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -1565,7 +1565,7 @@ void __cpuinit set_uncached_handler(unsigned long offset, void *addr,
 	unsigned long uncached_ebase = CKSEG1ADDR(ebase);
 
 	if (!addr)
-		panic(panic_null_cerr);
+		panic("%s", panic_null_cerr);
 
 	memcpy((void *)(uncached_ebase + offset), addr, size);
 }
