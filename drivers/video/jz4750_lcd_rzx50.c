350f0b96157cade3f65ee758a43a0917b118242c
Dmitry Smagin <dmitry.s.smagin@gmail.com>
2013-06-18 00:10:56 +0400

    lcd: JZ4750D: Some more cleanups for jz4750_lcd_a380.c and jz4750_lcd_rzx50.c

--- drivers/video/jz4750_lcd_rzx50.c
+++ drivers/video/jz4750_lcd_rzx50.c
@@ -2352,56 +2352,6 @@ static void display_v_color_bar(int w, int h, int bpp) {
 
 static void display_h_color_bar(int w, int h, int bpp)
 {
-  //int i,j, data = 0;
-  //int *ptr,*ptr2;
-  //int wpl; //word_per_line
-  //ptr = (int *)lcd_frame0;
-  //printk("========== Test H Color BAR ============\n");
-  //ptr2 = (int *)lcd_frame1;
-  //wpl = w*bpp/32;
-
-  //memset((unsigned char *)ptr, 320*240*2, 0x0F);
-  //dma_cache_wback((unsigned int)(ptr), 320 * 240 * 2);
-  //memset((unsigned char *)ptr2, 320*240*4, 0xF0);
-
-  //for(i=0; i<320*240; i++) Mcupanel_Data(0xF0F0);
-  printk("========== Test H Color BAR Over ============\n");
-#if 0
-  short open_pic0[400][240];
-  short open_pic1[400][240];
-  short open_pic2[400][240];
-  short open_pic3[400][240];
-  short open_pic4[400][240];
-  short open_pic5[400][240];
-  short open_pic6[400][240];
-  short open_pic7[400][240];
-  short open_pic8[400][240];
-  short open_pic9[400][240];
-#endif
-printk("====%d====== Test H Color BAR Over ============\n",__LINE__);
-#if 0
-  memset((char *)open_pic0,0x00,400*240*2);
- printk("====%d====== Test H Color BAR Over ============\n",__LINE__);
-  memset(open_pic1,0xf8,400*240*2);
-  printk("====%d====== Test H Color BAR Over ============\n",__LINE__);
- memset(open_pic2,0x20,400*240*2);
-  printk("====%d====== Test H Color BAR Over ============\n",__LINE__);
- memset(open_pic3,0x30,400*240*2);
-  printk("====%d====== Test H Color BAR Over ============\n",__LINE__);
- memset(open_pic4,0x01,400*240*2);
-  printk("====%d====== Test H Color BAR Over ============\n",__LINE__);
- memset(open_pic5,0x02,400*240*2);
-  printk("====%d====== Test H Color BAR Over ============\n",__LINE__);
- memset(open_pic6,0x03,400*240*2);
-  printk("====%d====== Test H Color BAR Over ============\n",__LINE__);
- memset(open_pic7,0x0f,400*240*2);
-  printk("====%d====== Test H Color BAR Over ============\n",__LINE__);
- memset(open_pic8,0xf0,400*240*2);
-  printk("====%d====== Test H Color BAR Over ============\n",__LINE__);
- memset(open_pic9,0xff,400*240*2);
-  printk("====%d====== Test H Color BAR Over ============\n",__LINE__);
-#endif
- //while(1);
   int i,j;
   unsigned short *p;
   int k = 0;
@@ -2433,85 +2383,6 @@ printk("====%d====== Test H Color BAR Over ============\n",__LINE__);
 printk("====%d====== Test H Color BAR Over ============\n",__LINE__);
 
 #endif
-
-#if 0
-  if (!(bpp > 8))
-    for (i = 0;i < wpl*h;i++) {
-      switch(bpp){
-        case 1:
-          if(i%(wpl*8)==0)
-            data = ((i/(wpl*8))%2)*0xffffffff;
-          *ptr++ = data;
-          break;
-        case 2:
-          if(i%(wpl*8)==0)
-            data = ((i/(wpl*8))%4)*0x55555555;
-          *ptr++ = data;
-          break;
-        case 4:
-          if(i%(wpl*8)==0)
-            data = ((i/(wpl*8))%16)*0x11111111;
-          *ptr++ = data;
-          break;
-        case 8:
-          if(i%(wpl*8)==0)
-            data = ((i/(wpl*8))%256)*0x01010101;
-          *ptr++ = data;
-          break;
-      }
-    }
-  else {
-
-    switch(bpp) {
-      case 15:
-      case 16:
-        for (i = 0;i < wpl*h;i++) {
-          if (((i/(wpl*8)) % 8) == 0)
-            *ptr++ = 0xffffffff;
-          else if (((i/(wpl*8)) % 8) == 1)
-            *ptr++ = 0xf800f800;
-          else if (((i/(wpl*8)) % 8) == 2)
-            *ptr++ = 0xffe0ffe0;
-          else if (((i/(wpl*8)) % 8) == 3)
-            *ptr++ = 0x07e007e0;
-          else if (((i/(wpl*8)) % 8) == 4)
-            *ptr++ = 0x07ff07ff;
-          else if (((i/(wpl*8)) % 8) == 5)
-            *ptr++ = 0x001f001f;
-          else if (((i/(wpl*8)) % 8) == 6)
-            *ptr++ = 0xf81ff81f;
-          else if (((i/(wpl*8)) % 8) == 7)
-            *ptr++ = 0x00000000;
-        }
-        break;
-      case 18:
-      case 24:
-      case 32:
-      default:
-        for (i = 0;i < wpl*h;i++) {
-          if (((i/(wpl*8)) % 8) == 7)
-            *ptr++ = 0xffffff;
-          else if (((i/(wpl*8)) % 8) == 2)
-            *ptr++ = 0xff0000;
-          else if (((i/(wpl*8)) % 8) == 4)
-            *ptr++ = 0xffff00;
-          else if (((i/(wpl*8)) % 8) == 6)
-            *ptr++ = 0x00ff00;
-          else if (((i/(wpl*8)) % 8) == 1)
-            *ptr++ = 0x00ffff;
-          else if (((i/(wpl*8)) % 8) == 3)
-            *ptr++ = 0x0000ff;
-          else if (((i/(wpl*8)) % 8) == 5)
-            *ptr++ = 0x000000;
-          else if (((i/(wpl*8)) % 8) == 0)
-            *ptr++ = 0xff00ff;
-        }
-        break;
-    }
-
-  }
-#endif
-
 }
 
 void draw_lock_picture(void)
@@ -2531,8 +2402,8 @@ void draw_lock_picture(void)
 			   (*q & 0xe000) == 0xe000 ||
 			   (*q & 0xd000) == 0xd000 ||
 			   (*q & 0xc000) == 0xc000) {
-				*p++;
-				*q++;
+				p++;
+				q++;
 			} else {
 				*p++ = *q++;
 			}
