c9e9bf9e59db90d15fcc5ff531f98aad25e196a2
Dmitry Smagin <dmitry.s.smagin@gmail.com>
2014-01-17 16:15:33 +0400

    oss: JZ4750D: Remove unused parts

--- sound/oss/jzdlv.c
+++ sound/oss/jzdlv.c
@@ -31,8 +31,25 @@
 
 #include "jz_audio.h"
 #include "jz_codec.h"
-#include "jz4750_dlv.h"
 #include "jz_i2s_dbg.h"
+
+#define DLV_MAX_RATE_COUNT	11
+
+void dlv_write_reg(int addr, int val);
+void dump_dlv_regs(const char *str);
+
+static void dlv_set_replay(void);
+//static void set_record_mic_input_audio_without_playback(void);
+//static void set_playback_line_input_audio_direct_only(void);
+//static void unset_playback_line_input_audio_direct_only(void);
+//static void set_record_mic_input_audio_with_direct_playback(void);
+//static void set_record_playing_audio_mixed_with_mic_input_audio(void);
+//static void unset_record_playing_audio_mixed_with_mic_input_audio(void);
+//static void set_record_mic_input_audio_with_audio_data_replay(void);
+//static void dlv_set_record_mic_with_replay(void);
+//static void unset_record_mic_input_audio_with_audio_data_replay(void);
+static void set_record_line_input_audio_with_audio_data_replay(void);
+//static void unset_record_line_input_audio_with_audio_data_replay(void);
 
 #define REPLAY		1
 #define RECORD		2
