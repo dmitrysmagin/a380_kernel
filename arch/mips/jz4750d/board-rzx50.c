348f0403d3410e7eb769af1c8b41f4cfc2efc3c3
Dmitry Smagin <exmortis@yandex.ru>
2014-01-23 16:44:00 +0400

    MIPS: JZ4750D: Cleanup MMC0 and MMC1 definitions

--- arch/mips/jz4750d/board-rzx50.c
+++ arch/mips/jz4750d/board-rzx50.c
@@ -39,29 +39,17 @@ struct jz4750d_clock_board_data jz4750d_clock_bdata = {
 
 void __init board_msc_init(void);
 
-#define CONFIG_JZ_ROOTFS_IN_SD
 static void cetus_sd_gpio_init(struct device *dev)
 {
 	__gpio_as_msc0_4bit();
-#ifndef CONFIG_JZ_ROOTFS_IN_SD
-	__gpio_as_output(GPIO_SD0_VCC_EN_N);
-	__gpio_as_input(GPIO_SD0_CD_N);
-#endif
-
 }
 
 static void cetus_sd_power_on(struct device *dev)
 {
-#ifndef CONFIG_JZ_ROOTFS_IN_SD
-	__gpio_clear_pin(GPIO_SD0_VCC_EN_N);
-#endif
 }
 
 static void cetus_sd_power_off(struct device *dev)
 {
-#ifndef CONFIG_JZ_ROOTFS_IN_SD
-	__gpio_set_pin(GPIO_SD0_VCC_EN_N);
-#endif
 }
 
 static void cetus_sd_cpm_start(struct device *dev)
@@ -71,84 +59,33 @@ static void cetus_sd_cpm_start(struct device *dev)
 
 static unsigned int cetus_sd_status(struct device *dev)
 {
-#ifndef CONFIG_JZ_ROOTFS_IN_SD
-	unsigned int status;
-	status = (unsigned int) __gpio_get_pin(GPIO_SD0_CD_N);
-	return (!status);
-#else
 	return 1;
-#endif
 }
 
 static void cetus_sd_plug_change(int state)
 {
-#ifndef CONFIG_JZ_ROOTFS_IN_SD
-	if(state == CARD_INSERTED)
-		__gpio_as_irq_rise_edge(MSC0_HOTPLUG_PIN);
-	else
-		__gpio_as_irq_fall_edge(MSC0_HOTPLUG_PIN);
-#endif
 }
 
 static unsigned int cetus_sd_get_wp(struct device *dev)
 {
-#ifndef CONFIG_JZ_ROOTFS_IN_SD
-	unsigned int status;
-
-	status = (unsigned int) __gpio_get_pin(GPIO_SD0_WP);
-	return (status);
-#else
 	return 0;
-#endif
 }
-#ifdef CONFIG_JZ_RECOVERY
-struct mmc_partition_info cetus_partitions[] = {
-	[0] = {"mbr",0,512,0},
-	[1] = {"uboot",512,3*1024*1024-512,0}, 
-	[2] = {"misc",0x300000,0x100000,0},
-	[3] = {"kernel",0x400000,0x400000,0},
-	[4] = {"recovery",0x800000,0x400000,0},
 
-	[5] = {"rootfs",12*1024*1024,100*1024*1024,1}, 
-	[6] = {"ebookfs",112*1024*1024,60*1024*1024,1},
-	[7] = {"qtefs",172*1024*1024,80*1024*1024,1},
-	[8] = {"data",252*1024*1024,3500*0x100000,0},
-};
-#endif
 struct jz_mmc_platform_data cetus_sd_data = {
-#ifndef CONFIG_JZ_MSC0_SDIO_SUPPORT
-	.support_sdio   = 0,
-#else
 	.support_sdio   = 1,
-#endif
-	.ocr_mask	= MMC_VDD_32_33 | MMC_VDD_33_34,
-#ifdef CONFIG_JZ_SYSTEM_AT_CARD
-	.status_irq	= 0,
+	.ocr_mask       = MMC_VDD_32_33 | MMC_VDD_33_34,
+	.status_irq     = 0,
 	.detect_pin     = 0,
 	.nonremovable   = 1,
-#else
-	.status_irq	= MSC0_HOTPLUG_IRQ,
-	.detect_pin     = GPIO_SD0_CD_N,
-#endif
 	.init           = cetus_sd_gpio_init,
 	.power_on       = cetus_sd_power_on,
 	.power_off      = cetus_sd_power_off,
-	.cpm_start	= cetus_sd_cpm_start,
-	.status		= cetus_sd_status,
-	.plug_change	= cetus_sd_plug_change,
+	.cpm_start      = cetus_sd_cpm_start,
+	.status         = cetus_sd_status,
+	.plug_change    = cetus_sd_plug_change,
 	.write_protect  = cetus_sd_get_wp,
 	.max_bus_width  = MMC_CAP_MMC_HIGHSPEED | MMC_CAP_SD_HIGHSPEED | MMC_CAP_4_BIT_DATA,
-#ifdef CONFIG_JZ_MSC0_BUS_1
-	.bus_width      = 1,
-#elif defined  CONFIG_JZ_MSC0_BUS_4
-	.bus_width      = 4,
-#else
-	.bus_width      = 8,
-#endif
-#ifdef CONFIG_JZ_RECOVERY
-	.partitions = cetus_partitions,
-	.num_partitions = ARRAY_SIZE(cetus_partitions),
-#endif
+	.bus_width      = 4, /* 1, 4 or 8 */
 };
 
 static void cetus_tf_gpio_init(struct device *dev)
@@ -190,26 +127,18 @@ static void cetus_tf_plug_change(int state)
 }
 
 struct jz_mmc_platform_data cetus_tf_data = {
-#ifndef CONFIG_JZ_MSC1_SDIO_SUPPORT
-	.support_sdio   = 0,
-#else
 	.support_sdio   = 1,
-#endif
-	.ocr_mask	= MMC_VDD_32_33 | MMC_VDD_33_34,
-	.status_irq	= MSC1_HOTPLUG_IRQ,
+	.ocr_mask       = MMC_VDD_32_33 | MMC_VDD_33_34,
+	.status_irq     = MSC1_HOTPLUG_IRQ,
 	.detect_pin     = GPIO_SD1_CD_N,
 	.init           = cetus_tf_gpio_init,
 	.power_on       = cetus_tf_power_on,
 	.power_off      = cetus_tf_power_off,
-	.cpm_start	= cetus_tf_cpm_start,
-	.status		= cetus_tf_status,
-	.plug_change	= cetus_tf_plug_change,
+	.cpm_start      = cetus_tf_cpm_start,
+	.status         = cetus_tf_status,
+	.plug_change    = cetus_tf_plug_change,
 	.max_bus_width  = MMC_CAP_SD_HIGHSPEED | MMC_CAP_4_BIT_DATA,
-#ifdef CONFIG_JZ_MSC1_BUS_1
 	.bus_width      = 1,
-#else
-	.bus_width      = 4,
-#endif
 };
 
 //////////////////////////////////////////////////////////
@@ -253,13 +182,8 @@ static struct platform_device *jz_platform_devices[] __initdata = {
 
 static int __init rzx50_init_platform_devices(void)
 {
-#ifdef CONFIG_JZ_MSC0
 	jz_msc0_device.dev.platform_data = &cetus_sd_data;
-#endif
-
-#ifdef CONFIG_JZ_MSC1
 	jz_msc1_device.dev.platform_data = &cetus_tf_data;
-#endif
 
 	jz4750d_serial_device_register();
 
@@ -289,4 +213,3 @@ static int __init jz_board_setup(void)
 }
 
 arch_initcall(jz_board_setup);
-
