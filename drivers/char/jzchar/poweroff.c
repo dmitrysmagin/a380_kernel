621832e7f136b1a85cc5fd0f4ace34ed9bae1dc4
Dmitry Smagin <exmortis@yandex.ru>
2014-04-04 22:14:48 +0400

    jzchar: JZ4750D: Cleanup poweroff.c; shutdown even if calling helper script failed.

--- drivers/char/jzchar/poweroff.c
+++ drivers/char/jzchar/poweroff.c
@@ -49,87 +49,31 @@ MODULE_LICENSE("GPL");
 
 //#define USE_SUSPEND_HOTPLUG
 
-#ifdef CONFIG_SOC_JZ4730
-#define GPIO_PW_I 97
-#define GPIO_PW_O 66
-#define POWEROFF_PIN_DOWN 1
-#define SET_POWEROFF_PIN_AS_IRQ __gpio_as_irq_rise_edge(POWEROFF_PIN)
-#define DO_SHUTDOWN_SYSTEM __gpio_clear_pin(GPIO_PW_O)
-#define DO_SUSPEND jz_pm_suspend()
-
-//#define GPIO_DISP_OFF_N   93
-#define __lcd_set_backlight_level(n) \
-do { \
-	REG_PWM_DUT(0) = n; \
-	REG_PWM_PER(0) = 7; \
-	REG_PWM_CTR(0) = 0x81; \
-} while (0)
-#define __lcd_close_backlight() \
-do { \
-	__lcd_set_backlight_level(0); \
-} while (0)
-#endif
-
-#ifdef CONFIG_SOC_JZ4740
-#define GPIO_PW_I 125
-#define POWEROFF_PIN_DOWN 0
-#define SET_POWEROFF_PIN_AS_IRQ __gpio_as_irq_fall_edge(POWEROFF_PIN)
-#define DO_SHUTDOWN_SYSTEM jz_pm_hibernate()
-#define DO_SUSPEND { \
-	    jz_pm_sleep();\
-	    suspend_flag = 0;\
-	    SET_POWEROFF_PIN_AS_IRQ;\
-	}
-
-//#define GPIO_DISP_OFF_N		118
-#define GPIO_PWM    123
-#define __lcd_close_backlight() \
-do { \
-__gpio_as_output(GPIO_PWM); \
-__gpio_clear_pin(GPIO_PWM); \
-} while (0)
-#endif
-
-#ifdef CONFIG_SOC_JZ4750
-#define GPIO_PW_I               GPIO_WAKEUP
-#define POWEROFF_PIN_DOWN       0
-#define SET_POWEROFF_PIN_AS_IRQ __gpio_as_irq_fall_edge(POWEROFF_PIN)
-#define DO_SHUTDOWN_SYSTEM      jz_pm_hibernate()
-#define DO_SUSPEND { \
-	    jz_pm_sleep();\
-	    suspend_flag = 0;\
-	    SET_POWEROFF_PIN_AS_IRQ;\
-	}
-#endif
-
 #ifdef CONFIG_SOC_JZ4750D
-#define GPIO_PW_I               GPIO_WAKEUP
-#define POWEROFF_PIN_DOWN       0
-#define SET_POWEROFF_PIN_AS_IRQ __gpio_as_irq_fall_edge(POWEROFF_PIN)
-#define DO_SHUTDOWN_SYSTEM      jz_pm_hibernate()
-#define DO_SUSPEND { \
-	    jz_pm_sleep();\
-	    suspend_flag = 0;\
-	    SET_POWEROFF_PIN_AS_IRQ;\
+#define GPIO_PW_I		GPIO_WAKEUP
+#define POWEROFF_PIN_DOWN	0
+#define SET_POWEROFF_PIN_AS_IRQ	__gpio_as_irq_fall_edge(POWEROFF_PIN)
+#define DO_SHUTDOWN_SYSTEM	jz_pm_hibernate()
+#define DO_SUSPEND {				\
+		jz_pm_sleep();			\
+		suspend_flag = 0;		\
+		SET_POWEROFF_PIN_AS_IRQ;	\
 	}
 #endif
 
+#define POWEROFF_PIN	GPIO_PW_I
+#define POWEROFF_IRQ	(IRQ_GPIO_0 + POWEROFF_PIN)
 
-#define POWEROFF_PIN   GPIO_PW_I
-#define POWEROFF_IRQ   (IRQ_GPIO_0 + POWEROFF_PIN)
+#define POWEROFF_PERIOD		1000    /* unit: ms */
+#define POWEROFF_DELAY		100     /* unit: ms */
 
-#define POWEROFF_PERIOD         1000    /* unit: ms */
-#define POWEROFF_DELAY          100     /* unit: ms */
-//KJH ADD
 static struct task_struct *poweroff_task;
-static struct timer_list poweroff_timer;
 static struct timer_list poweroff_delaytimer;
 static struct timer_list poweroff_checktimer;
 static struct work_struct suspend_work;
 
 static int poweroff_flag = 0;
 static int suspend_flag = 0;
-static int num_seconds = 0;
 
 #ifdef CONFIG_JZ_UDC_HOTPLUG
 extern int jz_udc_active;
@@ -139,129 +83,53 @@ extern void jz_pm_suspend(void);
 extern int jz_pm_hibernate(void);
 extern int jz_pm_sleep(void);
 
-extern unsigned int medive_flag;
-//maddrone add
-void run_sbin_poweroff()
+void run_sbin_poweroff(void)
 {
-	int i;
+	int i = 0;
 	char *argv[3], *envp[8];
-    int ret;
+	int ret;
 
-    i = 0;
-    //if (medive_flag == 1){
-   if(1){
-	    argv[i++] = "/boot/local/sbin/ne_inform";
-	    //argv[i++] = "/boot/local/sbin/ne_inform.sh";
-	    //argv[i++] = "/usr/bin/udc_connect.sh";
+	argv[i++] = "/boot/local/sbin/ne_inform";
+	argv[i++] = "poweroff";
+	argv[i] = 0;
 
-	    argv[i++] = "poweroff";
+	/* minimal command environment */
+	i = 0;
+	envp[i++] = "HOME=/";
+	envp[i++] = "PATH=/sbin:/bin:/usr/sbin:/usr/bin:/boot/local/sbin";
 
-	    argv[i] = 0;
+	envp[i] = 0;
 
-	    /* minimal command environment */
-	    i = 0;
-	    envp[i++] = "HOME=/";
-	    envp[i++] = "PATH=/sbin:/bin:/usr/sbin:/usr/bin:/boot/local/sbin";
+	printk("Call user poweroff helper..!\n");
+	ret = call_usermodehelper(argv[0], argv, envp, 1);
 
-	    envp[i] = 0;
+	if(ret != 0)
+		printk("Call user poweroff helper error, errno = %d\n",ret);
+	else
+		printk("%s %d  %s Call user poweroff helper ok..!\n",__FILE__,__LINE__,argv[0]);
 
-	    printk("call user helper..!\n");
-	    ret = call_usermodehelper (argv[0], argv, envp, 1);
-	    if(ret != 0)
-	    {
-		    printk("call user helper error, errno = %d\n",ret);
-		    SET_POWEROFF_PIN_AS_IRQ;
-		    __gpio_unmask_irq(POWEROFF_PIN);
-		    return;
-	    }
-	    else
-	    {
-		    printk("%s %d  %s call user helper ok..!\n",__FILE__,__LINE__,argv[0]);
-            }
-            printk("\n  %s %d\n",__FILE__,__LINE__);
-            msleep(10*1000);
-	    printk("\n Medive pintk: rerun jz pm hibernate!\n");
-	    jz_pm_hibernate();
-    }else{
-	    printk("Medive printk: Can't do hibernate! please wait!\n");
-    }
-    //del_timer(&poweroff_delaytimer);
-    SET_POWEROFF_PIN_AS_IRQ;
-    __gpio_unmask_irq(POWEROFF_PIN);
+	jz_pm_hibernate();
+
+	SET_POWEROFF_PIN_AS_IRQ;
+	__gpio_unmask_irq(POWEROFF_PIN);
 }
 EXPORT_SYMBOL(run_sbin_poweroff);
 
-
-static void poweroff_timer_routine(unsigned long dummy)
-{
-#if 0
-	if (__gpio_get_pin(POWEROFF_PIN) == POWEROFF_PIN_DOWN) {
-		if (++num_seconds > 3)
-		{
-			printk("\nShutdown system now ..\n");
-
-#ifndef USE_SUSPEND_HOTPLUG
-                        /* Turn off LCD to inform user that the system is shutting down.
-                         * But the information of shutting down system will be shown
-                         * by userspace program if hotplug is used.
-                         */
-			__lcd_close_backlight();
-#endif
-
-                        /*
-                         * Wait until the power key is up, or the system will reset with
-                         * power key down after entering hibernate.
-                         */
-			while(__gpio_get_pin(POWEROFF_PIN)==POWEROFF_PIN_DOWN);
-
-			poweroff_flag = 1;
-			schedule_work(&suspend_work); /* inform user to poweroff */
-		}
-		else {
-			del_timer(&poweroff_timer);
-			init_timer(&poweroff_timer);
-			poweroff_timer.expires = jiffies + POWEROFF_PERIOD/10;
-			poweroff_timer.data = 0;
-			poweroff_timer.function = poweroff_timer_routine;
-			add_timer(&poweroff_timer);
-		}
-	}
-	else
-	{
-		printk("\nSuspend system now ..\n");
-		num_seconds = 0;
-		suspend_flag = 1;
-		poweroff_flag = 0;
-		schedule_work(&suspend_work); /* we are entering suspend */
-	}
-#endif
-//maddrone add
-printk("\nShutdown system now ..\n");
-mdelay(280);
-while(__gpio_get_pin(POWEROFF_PIN)==POWEROFF_PIN_DOWN);
-poweroff_flag = 1;
-schedule_work(&suspend_work); /* inform user to poweroff */
-}
-
 static void poweroff_timer_check(unsigned long dummy)
 {
-	if (__gpio_get_pin(POWEROFF_PIN)!=POWEROFF_PIN_DOWN)
-	{
+	if(__gpio_get_pin(POWEROFF_PIN)!=POWEROFF_PIN_DOWN) {
 		del_timer(&poweroff_checktimer);
 		wake_up_process(poweroff_task);
-	}
-	else
-	{
+	} else {
 		mod_timer(&poweroff_checktimer, jiffies + POWEROFF_PERIOD / 100);
 	}
-
 }
 
 static void poweroff_delaytimer_routine(unsigned long dummy)
 {
 	__gpio_as_input(POWEROFF_PIN);
-	if (__gpio_get_pin(POWEROFF_PIN)==POWEROFF_PIN_DOWN) {
-		if (suspend_flag) {
+	if(__gpio_get_pin(POWEROFF_PIN)==POWEROFF_PIN_DOWN) {
+		if(suspend_flag) {
 			suspend_flag = 0;
 			del_timer(&poweroff_delaytimer);
 			SET_POWEROFF_PIN_AS_IRQ;
@@ -278,17 +146,7 @@ static void poweroff_delaytimer_routine(unsigned long dummy)
 		poweroff_checktimer.data = 0;
 		poweroff_checktimer.function = poweroff_timer_check;
 		add_timer(&poweroff_checktimer);
-#if 0
-		del_timer(&poweroff_delaytimer);
-		del_timer(&poweroff_timer);
-		init_timer(&poweroff_timer);
-		poweroff_timer.expires = jiffies + POWEROFF_PERIOD/100;
-		poweroff_timer.data = 0;
-		poweroff_timer.function = poweroff_timer_routine;
-		add_timer(&poweroff_timer);
-#endif
-	}
-	else {
+	} else {
 		del_timer(&poweroff_delaytimer);
 		SET_POWEROFF_PIN_AS_IRQ;
 		__gpio_unmask_irq(POWEROFF_PIN);
@@ -307,19 +165,17 @@ static irqreturn_t poweroff_irq(int irq, void *dev_id)
         __gpio_as_input(POWEROFF_PIN);
 
 #ifdef CONFIG_JZ_UDC_HOTPLUG
-	if (__gpio_get_pin(POWEROFF_PIN)==POWEROFF_PIN_DOWN && jz_udc_active == 0){
+	if (__gpio_get_pin(POWEROFF_PIN)==POWEROFF_PIN_DOWN && jz_udc_active == 0) {
 #else
-	if (__gpio_get_pin(POWEROFF_PIN)==POWEROFF_PIN_DOWN){
+	if (__gpio_get_pin(POWEROFF_PIN)==POWEROFF_PIN_DOWN) {
 #endif
 		del_timer(&poweroff_delaytimer);
 		init_timer(&poweroff_delaytimer);
 		poweroff_delaytimer.expires = jiffies + POWEROFF_DELAY;
-		//poweroff_delaytimer.expires = jiffies + 2 * HZ;
 		poweroff_delaytimer.data = 0;
 		poweroff_delaytimer.function = poweroff_delaytimer_routine;
 		add_timer(&poweroff_delaytimer);
-	}
-	else {
+	} else {
 
 /*
  * If it reaches here without jz_udc_active == 0, then it indicates POWEROFF_PIN was
@@ -329,25 +185,20 @@ static irqreturn_t poweroff_irq(int irq, void *dev_id)
 
 #ifdef CONFIG_JZ_UDC_HOTPLUG
 		if (jz_udc_active == 1)
-                  printk("\nUSB is working; Operation is denied\n");
+			printk("\nUSB is working; Operation is denied\n");
 
 #endif
-
-                if(!__gpio_get_pin(GPIO_USB_DETE))
-                {
-                  del_timer(&poweroff_delaytimer);
-                  init_timer(&poweroff_delaytimer);
-                  poweroff_delaytimer.expires = jiffies + POWEROFF_DELAY;
-                  //poweroff_delaytimer.expires = jiffies + 2 * HZ;
-                  poweroff_delaytimer.data = 0;
-                  poweroff_delaytimer.function = poweroff_delaytimer_routine;
-                  add_timer(&poweroff_delaytimer);
-                }
-                else
-                {
-                  SET_POWEROFF_PIN_AS_IRQ;
-                  __gpio_unmask_irq(POWEROFF_PIN);
-                }
+		if(!__gpio_get_pin(GPIO_USB_DETE)) {
+			del_timer(&poweroff_delaytimer);
+			init_timer(&poweroff_delaytimer);
+			poweroff_delaytimer.expires = jiffies + POWEROFF_DELAY;
+			poweroff_delaytimer.data = 0;
+			poweroff_delaytimer.function = poweroff_delaytimer_routine;
+			add_timer(&poweroff_delaytimer);
+		} else 	{
+			SET_POWEROFF_PIN_AS_IRQ;
+			__gpio_unmask_irq(POWEROFF_PIN);
+		}
 	}
 
 	return IRQ_HANDLED;
@@ -403,51 +254,50 @@ static int poweroff_pm_callback(struct pm_dev *pm_dev, pm_request_t rqst, void *
 #ifdef USE_SUSPEND_HOTPLUG
 static void run_sbin_hotplug(int state)
 {
-        int i;
-        char *argv[3], *envp[8];
-        char media[64], slotnum[16];
-        if (!uevent_helper[0])
-                return;
+	int i = 0;
+	char *argv[3], *envp[8];
+	char media[64], slotnum[16];
+	if (!uevent_helper[0]) return;
 
-        i = 0;
 	argv[i++] = uevent_helper;
- 	//argv[i++] = "home/lhhuang/hotplug";
+	//argv[i++] = "home/lhhuang/hotplug";
 
-	if ( poweroff_flag ==  1 )
-        	argv[i++] = "poweroff";
+	if(poweroff_flag == 1)
+		argv[i++] = "poweroff";
 	else
-        	argv[i++] = "suspend";
+		argv[i++] = "suspend";
 
         argv[i] = 0;
 
-        /* minimal command environment */
-        i = 0;
-        envp[i++] = "HOME=/";
-        envp[i++] = "PATH=/sbin:/bin:/usr/sbin:/usr/bin";
+	/* minimal command environment */
+	i = 0;
+	envp[i++] = "HOME=/";
+	envp[i++] = "PATH=/sbin:/bin:/usr/sbin:/usr/bin";
 
-        /* other stuff we want to pass to /sbin/hotplug */
-        sprintf(slotnum, "SLOT=0");
+	/* other stuff we want to pass to /sbin/hotplug */
+	sprintf(slotnum, "SLOT=0");
 
-	if ( poweroff_flag ==  1 )
-        	sprintf(media, "MEDIA=poweroff");
+	if (poweroff_flag == 1)
+		sprintf(media, "MEDIA=poweroff");
 	else
-        	sprintf(media, "MEDIA=suspend");
+		sprintf(media, "MEDIA=suspend");
 
-        envp[i++] = slotnum;
-        envp[i++] = media;
+	envp[i++] = slotnum;
+	envp[i++] = media;
 
-       	if (state)
-               	envp[i++] = "ACTION=enter";
-       	else
-               	envp[i++] = "ACTION=exit";
+	if(state)
+		envp[i++] = "ACTION=enter";
+	else
+		envp[i++] = "ACTION=exit";
 
-        envp[i] = 0;
+	envp[i] = 0;
 
-        dprintk("SUSPEND: hotplug path=%s state=%d\n", argv[0], state);
+	dprintk("SUSPEND: hotplug path=%s state=%d\n", argv[0], state);
 
+	/* set it because call hotplug with call_usermodehelper() 
+	   might failed, especially when using nfsroot */
 	SET_POWEROFF_PIN_AS_IRQ;
-	__gpio_unmask_irq(POWEROFF_PIN); /* set it because call hotplug with call_usermodehelper() \
-                                           might failed, especially when using nfsroot */
+	__gpio_unmask_irq(POWEROFF_PIN); 
 
 	call_usermodehelper (argv [0], argv, envp, -1);
 }
@@ -456,32 +306,22 @@ static void run_sbin_hotplug(int state)
 static void suspend_handler(struct work_struct *work)
 {
 #ifdef USE_SUSPEND_HOTPLUG
-        int state = 1;
-        run_sbin_hotplug(state);
+	int state = 1;
+	run_sbin_hotplug(state);
 #else
-//maddrone changed
-#if 0
-	if (poweroff_flag) {
-		dprintk("DO_SHUTDOWN_SYSTEM\n");
-		DO_SHUTDOWN_SYSTEM;
-	} else {
-		dprintk("DO_SUSPEND\n");
-		DO_SUSPEND;
-	}
-#endif
 	DO_SHUTDOWN_SYSTEM;
 #endif
 }
 
 static int poweroff_thread(void *unused)
 {
-	while(1)
-	{
+	while(1) {
 		set_current_state(TASK_UNINTERRUPTIBLE);
 		schedule();
 		printk("poweroff wakeup\n");
 		run_sbin_poweroff();
 	}
+	return 0;
 }
 
 static int __init poweroff_init(void)
