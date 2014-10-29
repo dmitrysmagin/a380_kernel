0e07355ddb420ee56af804e68248695781b1c646
Dmitry Smagin <dmitry.s.smagin@gmail.com>
2014-10-29 13:37:42 +0400

    MIPS: JZ4750D: Modernize irq to kernel 2.6.37

--- arch/mips/jz4750d/irq.c
+++ arch/mips/jz4750d/irq.c
@@ -14,6 +14,7 @@
 #include <linux/errno.h>
 #include <linux/init.h>
 #include <linux/irq.h>
+#include <linux/irqdesc.h>
 #include <linux/kernel_stat.h>
 #include <linux/module.h>
 #include <linux/signal.h>
@@ -40,56 +41,107 @@
  * INTC irq type
  */
 
-static void enable_intc_irq(unsigned int irq)
+static void enable_intc_irq(struct irq_data *data)
 {
-	__intc_unmask_irq(irq);
+	__intc_unmask_irq(data->irq);
 }
 
-static void disable_intc_irq(unsigned int irq)
+static void disable_intc_irq(struct irq_data *data)
 {
-	__intc_mask_irq(irq);
+	__intc_mask_irq(data->irq);
 }
 
-static void mask_and_ack_intc_irq(unsigned int irq)
+static void mask_and_ack_intc_irq(struct irq_data *data)
 {
-	__intc_mask_irq(irq);
-	__intc_ack_irq(irq);
+	__intc_mask_irq(data->irq);
+	__intc_ack_irq(data->irq);
 }
 
-static void end_intc_irq(unsigned int irq)
+static unsigned int startup_intc_irq(struct irq_data *data)
 {
-	if (!(irq_desc[irq].status & (IRQ_DISABLED|IRQ_INPROGRESS))) {
-		enable_intc_irq(irq);
-	}
-}
-
-static unsigned int startup_intc_irq(unsigned int irq)
-{
-	enable_intc_irq(irq);
+	enable_intc_irq(data);
 	return 0;
 }
 
-static void shutdown_intc_irq(unsigned int irq)
+static void shutdown_intc_irq(struct irq_data *data)
 {
-	disable_intc_irq(irq);
+	disable_intc_irq(data);
 }
 
 static struct irq_chip intc_irq_type = {
-	.typename = "INTC",
-	.startup = startup_intc_irq,
-	.shutdown = shutdown_intc_irq,
-	.unmask = enable_intc_irq,
-	.mask = disable_intc_irq,
-	.ack = mask_and_ack_intc_irq,
-	.end = end_intc_irq,
+	.name = "INTC",
+	.irq_startup = startup_intc_irq,
+	.irq_shutdown = shutdown_intc_irq,
+	.irq_unmask = enable_intc_irq,
+	.irq_mask = disable_intc_irq,
+	.irq_ack = mask_and_ack_intc_irq,
+};
+
+/*
+ * GPIO irq type
+ */
+
+static void enable_gpio_irq(struct irq_data *data)
+{
+	unsigned int irq = data->irq;
+	unsigned int intc_irq;
+
+	if (irq < (IRQ_GPIO_0 + 32)) {
+		intc_irq = IRQ_GPIO0;
+	} else if (irq < (IRQ_GPIO_0 + 64)) {
+		intc_irq = IRQ_GPIO1;
+	} else if (irq < (IRQ_GPIO_0 + 96)) {
+		intc_irq = IRQ_GPIO2;
+	} else if (irq < (IRQ_GPIO_0 + 128)) {
+		intc_irq = IRQ_GPIO3;
+	} else if (irq < (IRQ_GPIO_0 + 160)) {
+		intc_irq = IRQ_GPIO4;
+	} else {
+		intc_irq = IRQ_GPIO5;
+	}
+
+	enable_intc_irq(&irq_desc[intc_irq].irq_data);
+	__gpio_unmask_irq(irq - IRQ_GPIO_0);
+}
+
+static void disable_gpio_irq(struct irq_data *data)
+{
+	__gpio_mask_irq(data->irq - IRQ_GPIO_0);
+}
+
+static void mask_and_ack_gpio_irq(struct irq_data *data)
+{
+	__gpio_mask_irq(data->irq - IRQ_GPIO_0);
+	__gpio_ack_irq(data->irq - IRQ_GPIO_0);
+}
+
+static unsigned int startup_gpio_irq(struct irq_data *data)
+{
+	enable_gpio_irq(data);
+	return 0;
+}
+
+static void shutdown_gpio_irq(struct irq_data *data)
+{
+	disable_gpio_irq(data);
+}
+
+static struct irq_chip gpio_irq_type = {
+	.name = "GPIO",
+	.irq_startup = startup_gpio_irq,
+	.irq_shutdown = shutdown_gpio_irq,
+	.irq_unmask = enable_gpio_irq,
+	.irq_mask = disable_gpio_irq,
+	.irq_ack = mask_and_ack_gpio_irq,
 };
 
 /*
  * DMA irq type
  */
 
-static void enable_dma_irq(unsigned int irq)
+static void enable_dma_irq(struct irq_data *data)
 {
+	unsigned int irq = data->irq;
 	unsigned int intc_irq;
 
 	if ( irq < (IRQ_DMA_0 + HALF_DMA_NUM) ) 	/* DMAC Group 0 irq */
@@ -100,22 +152,23 @@ static void enable_dma_irq(unsigned int irq)
 		printk("%s, unexpected dma irq #%d\n", __FILE__, irq);
 		return;
 	}
-	__intc_unmask_irq(intc_irq);
+	__intc_unmask_irq(irq);
 	__dmac_channel_enable_irq(irq - IRQ_DMA_0);
 }
 
-static void disable_dma_irq(unsigned int irq)
+static void disable_dma_irq(struct irq_data *data)
 {
-	int chan = irq - IRQ_DMA_0;
+	int chan = data->irq - IRQ_DMA_0;
 	__dmac_disable_channel(chan);
 	__dmac_channel_disable_irq(chan);
 }
 
-static void mask_and_ack_dma_irq(unsigned int irq)
+static void mask_and_ack_dma_irq(struct irq_data *data)
 {
+	unsigned int irq = data->irq;
 	unsigned int intc_irq;
 
-	disable_dma_irq(irq);
+	disable_dma_irq(data);
 
 	if ( irq < (IRQ_DMA_0 + HALF_DMA_NUM) ) 	/* DMAC Group 0 irq */
 		intc_irq = IRQ_DMAC0;
@@ -125,37 +178,29 @@ static void mask_and_ack_dma_irq(unsigned int irq)
 		printk("%s, unexpected dma irq #%d\n", __FILE__, irq);
 		return ;
 	}
-	__intc_ack_irq(intc_irq);
+	__intc_ack_irq(irq);
 	//__dmac_channel_ack_irq(irq-IRQ_DMA_0); /* needed?? add 20080506, Wolfgang */
 	//__dmac_channel_disable_irq(irq - IRQ_DMA_0);
 }
 
-static void end_dma_irq(unsigned int irq)
+static unsigned int startup_dma_irq(struct irq_data *data)
 {
-	if (!(irq_desc[irq].status & (IRQ_DISABLED|IRQ_INPROGRESS))) {
-		enable_dma_irq(irq);
-	}
-}
-
-static unsigned int startup_dma_irq(unsigned int irq)
-{
-	enable_dma_irq(irq);
+	enable_dma_irq(data);
 	return 0;
 }
 
-static void shutdown_dma_irq(unsigned int irq)
+static void shutdown_dma_irq(struct irq_data *data)
 {
-	disable_dma_irq(irq);
+	disable_dma_irq(data);
 }
 
 static struct irq_chip dma_irq_type = {
-	.typename = "DMA",
-	.startup = startup_dma_irq,
-	.shutdown = shutdown_dma_irq,
-	.unmask = enable_dma_irq,
-	.mask = disable_dma_irq,
-	.ack = mask_and_ack_dma_irq,
-	.end = end_dma_irq,
+	.name = "DMA",
+	.irq_startup = startup_dma_irq,
+	.irq_shutdown = shutdown_dma_irq,
+	.irq_unmask = enable_dma_irq,
+	.irq_mask = disable_dma_irq,
+	.irq_ack = mask_and_ack_dma_irq,
 };
 
 //----------------------------------------------------------------------
@@ -169,31 +214,27 @@ void __init arch_init_irq(void)
 
 	/* Set up INTC irq */
 	for (i = 0; i < NUM_INTC; i++) {
-		disable_intc_irq(i);
+		disable_intc_irq(&irq_desc[i].irq_data);
 		set_irq_chip_and_handler(i, &intc_irq_type, handle_level_irq);
 	}
 	
 	/* Set up DMAC irq */
-	for (i = 0; i < NUM_DMA; i++) {
-		disable_dma_irq(IRQ_DMA_0 + i);
-		set_irq_chip_and_handler(IRQ_DMA_0 + i, &dma_irq_type, handle_level_irq);
+	for (i = IRQ_DMA_0; i < IRQ_DMA_0 + NUM_DMA; i++) {
+		disable_dma_irq(&irq_desc[i].irq_data);
+		set_irq_chip_and_handler(i, &dma_irq_type, handle_level_irq);
 	}
 
-	/*
-	 * Disable GPIO irqs here to be sure
-	 * Useful when running kernel with kexec
-	 */
-	for (i = 0; i < NUM_GPIO; i++) {
-		__gpio_mask_irq(i);
+	/* Set up GPIO irq */
+	for (i = IRQ_GPIO_0; i < IRQ_GPIO_0 + NUM_GPIO; i++) {
+		disable_gpio_irq(&irq_desc[i].irq_data);
+		set_irq_chip_and_handler(i, &gpio_irq_type, handle_level_irq);
 	}
 }
 
 static int plat_real_irq(int irq)
 {
-	int group = 0;
-
 	if ((irq >= IRQ_GPIO5) && (irq <= IRQ_GPIO0)) {
-		group = IRQ_GPIO0 - irq;
+		int group = IRQ_GPIO0 - irq;
 		irq = __gpio_group_irq(group);
 		if (irq >= 0)
 			irq += IRQ_GPIO_0 + 32 * group;
@@ -203,8 +244,10 @@ static int plat_real_irq(int irq)
 		case IRQ_DMAC1:
 			irq = __dmac_get_irq();
 			if (irq < 0) {
-				printk("REG_DMAC_DMAIPR(0) = 0x%08x\n", REG_DMAC_DMAIPR(0));
-				printk("REG_DMAC_DMAIPR(1) = 0x%08x\n", REG_DMAC_DMAIPR(1));
+				printk("REG_DMAC_DMAIPR(0) = 0x%08x\n",
+					REG_DMAC_DMAIPR(0));
+				printk("REG_DMAC_DMAIPR(1) = 0x%08x\n",
+					REG_DMAC_DMAIPR(1));
 				return irq;
 			}
 			irq += IRQ_DMA_0;
@@ -220,7 +263,7 @@ asmlinkage void plat_irq_dispatch(void)
 	int irq;
 	unsigned long intc_ipr = REG_INTC_IPR;
 
-	if (intc_ipr) { 
+	if (intc_ipr) {
 		irq = ffs(intc_ipr) - 1;
 	} else {
 		spurious_interrupt();
@@ -230,4 +273,3 @@ asmlinkage void plat_irq_dispatch(void)
 	irq = plat_real_irq(irq);
 	do_IRQ(irq);
 }
-
