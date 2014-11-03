539e45072c58f55a4e7aefc5d79d673c59a443eb
Dmitry Smagin <exmortis@yandex.ru>
2014-11-03 17:02:50 +0300

    MIPS: JZ4750D: Add platform code for JZ4755D and a380/rzx50 boards
    MIPS: JZ4750D: Add pwm platform code
    
    MIPS: JZ4750D: Add platform definitions for pwm-backlight
    
    MIPS: JZ4750D: Fix lcd power gpio pin for RZX50
    
    MIPS: JZ4750D: Fix pwm (hopefully)
    
    MIPS: JZ4750D: Remove legacy devices
    
    MIPS: JZ4750: Make pwm really work and fix gpio init for a380 keys
    
    MIPS: JZ4750D: Remove JZSOC and JZRISC defines
    
    MIPS: JZ4750D: SOC_JZ4750D => MACH_JZ4750D
    
    MIPS: JZ4750D: Fix id for jz-udc device
    
    MIPS: JZ4750D: Remove some unused /proc/jz interfaces
    
      Removed:
        /proc/jz/udc, /proc/jz/mmc, /proc/jz/ipu and /proc/jz/imem
    
      Left for debugging:
        /proc/jz/emc, /proc/jz/pmc and /proc/jz/cgm
    
    MIPS: JZ4750D: Remove disabling gpios from pm.c
    
    MIPS: JZ4750D: Remove i2c.c from platform code
    
    MIPS: JZ4750D: Remove backlight macros from board-***.h headers
    
    MIPS: JZ4750D: Add preliminary A320E board code
    
    MIPS: JZ4750D: Modernize irq to kernel 2.6.37
    
    MIPS: JZ4750D: Fix dma irq unmask and ack (this fixes jz-mmc)
    
    MIPS: JZ4750D: Fix offset for jz4750 codec
    
    MIPS: JZ4750D: Enable sound devices for a380
    
    MIPS: JZ4750D: Rename jz4750-pcm -> jz4750-pcm-audio
    
    Conflicts:
    	arch/mips/Kbuild.platforms
    	arch/mips/Kconfig
    
    MIPS: JZ4750D: Update irq and gpio code to kernel 2.6.39
    
    MIPS: JZ4750D: Adapt gpio code for kernel 3.0
    
    MIPS: JZ4750D: Correct fields for aic/i2s register I2SCR
    
      In fact bits 1..3 are reserved, remove any macros that touch them.
      Also the ESCLK field (bit 4) was missing for some reason which enables SYSCLK for internal jz4750 codec.
      Add macros __i2s_enable_sysclk() and __i2s_disable_sysclk()
    
    MIPS: JZ4750D: Fix macro __cpm_start_aic() and __cpm_stop_aic()
    
      Remove remnants from jz4740 which had 2 separate clocks for AIC/I2S

---
+++ arch/mips/jz4750d/board-rzx50.c
@@ -0,0 +1,300 @@
+/*
+ * linux/arch/mips/jz4750d/board-rzx50.c
+ *
+ * Ritmix RZX50 board setup routines.
+ *
+ * Copyright (c) 2006-2008  Ingenic Semiconductor Inc.
+ * Author: <jlwei@ingenic.cn>
+ * Mod: <maddrone@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/mmc/host.h>
+#include <linux/input.h>
+#include <linux/gpio_keys.h>
+#include <linux/pwm_backlight.h>
+#include <linux/input/matrix_keypad.h>
+
+#include <asm/mach-jz4750d/board-rzx50.h>
+#include <asm/mach-jz4750d/jz4750d_gpio.h>
+#include <asm/mach-jz4750d/jz4750d_intc.h>
+
+#include <asm/mach-jz4750d/jz4750d_mmc.h>
+#include <asm/mach-jz4750d/gpio.h>
+#include <asm/mach-jz4750d/platform.h>
+
+#include "serial.h"
+#include "clock.h"
+
+struct jz4750d_clock_board_data jz4750d_clock_bdata = {
+	.ext_rate = 24000000,
+	.rtc_rate = 32768,
+};
+
+void __init board_msc_init(void);
+
+static void cetus_sd_gpio_init(struct device *dev)
+{
+	__gpio_as_msc0_4bit();
+}
+
+static void cetus_sd_power_on(struct device *dev)
+{
+}
+
+static void cetus_sd_power_off(struct device *dev)
+{
+}
+
+static unsigned int cetus_sd_status(struct device *dev)
+{
+	return 1;
+}
+
+static void cetus_sd_plug_change(int state)
+{
+}
+
+static unsigned int cetus_sd_get_wp(struct device *dev)
+{
+	return 0;
+}
+
+struct jz_mmc_platform_data cetus_sd_data = {
+	.support_sdio   = 1,
+	.ocr_mask       = MMC_VDD_32_33 | MMC_VDD_33_34,
+	.status_irq     = 0,
+	.detect_pin     = 0,
+	.nonremovable   = 1,
+	.init           = cetus_sd_gpio_init,
+	.power_on       = cetus_sd_power_on,
+	.power_off      = cetus_sd_power_off,
+	.status         = cetus_sd_status,
+	.plug_change    = cetus_sd_plug_change,
+	.write_protect  = cetus_sd_get_wp,
+	.max_bus_width  = MMC_CAP_MMC_HIGHSPEED | MMC_CAP_SD_HIGHSPEED |
+			  MMC_CAP_4_BIT_DATA,
+	.bus_width      = 4, /* 1, 4 or 8 */
+};
+
+static void cetus_tf_gpio_init(struct device *dev)
+{
+	__gpio_as_msc1_1bit();
+	__gpio_as_output(GPIO_SD1_VCC_EN_N);
+	__gpio_as_input(GPIO_SD1_CD_N);
+}
+
+static void cetus_tf_power_on(struct device *dev)
+{
+	__msc1_enable_power();
+}
+
+static void cetus_tf_power_off(struct device *dev)
+{
+	__msc1_disable_power();
+}
+
+static unsigned int cetus_tf_status(struct device *dev)
+{
+	unsigned int status;
+
+	status = (unsigned int) __gpio_get_pin(GPIO_SD1_CD_N);
+	return (!status);
+}
+
+static void cetus_tf_plug_change(int state)
+{
+	if(state == CARD_INSERTED)
+		__gpio_as_irq_fall_edge(MSC1_HOTPLUG_PIN);
+	else
+		__gpio_as_irq_rise_edge(MSC1_HOTPLUG_PIN);
+}
+
+struct jz_mmc_platform_data cetus_tf_data = {
+	.support_sdio   = 1,
+	.ocr_mask       = MMC_VDD_32_33 | MMC_VDD_33_34,
+	.status_irq     = MSC1_HOTPLUG_IRQ,
+	.detect_pin     = GPIO_SD1_CD_N,
+	.init           = cetus_tf_gpio_init,
+	.power_on       = cetus_tf_power_on,
+	.power_off      = cetus_tf_power_off,
+	.status         = cetus_tf_status,
+	.plug_change    = cetus_tf_plug_change,
+	.max_bus_width  = MMC_CAP_SD_HIGHSPEED | MMC_CAP_4_BIT_DATA,
+	.bus_width      = 1,
+};
+
+/* Keys mapped to gpio */
+static struct gpio_keys_button rzx50_buttons[] = {
+	/* Left shift */ {
+		.gpio			= JZ_GPIO_PORTE(11),
+		.active_low		= 1,
+		.code			= KEY_TAB,
+		.debounce_interval	= 5,
+	},
+	/* POWER slider */ {
+		.gpio			= JZ_GPIO_PORTE(30),
+		.active_low		= 1,
+		.code			= KEY_POWER,
+		.wakeup			= 1,
+	},
+	/* POWER hold */ {
+		.gpio			= JZ_GPIO_PORTE(3),
+		.active_low		= 1,
+		.code			= KEY_PAUSE,
+	},
+};
+
+static struct gpio_keys_platform_data rzx50_gpio_keys_pdata = {
+	.buttons = rzx50_buttons,
+	.nbuttons = ARRAY_SIZE(rzx50_buttons),
+	.rep = 1,
+};
+
+static struct platform_device rzx50_gpio_keys_device = {
+	.name = "gpio-keys",
+	.id = -1,
+	.dev = {
+		.platform_data = &rzx50_gpio_keys_pdata,
+	},
+};
+
+/* Keys grouped in matrix */
+static const uint32_t board_matrix_keys[] = {
+	KEY(0, 0, KEY_UP),
+	KEY(0, 1, KEY_LEFT),
+	KEY(0, 2, KEY_RIGHT),
+	KEY(0, 3, KEY_DOWN),
+
+	KEY(1, 0, KEY_ENTER),		/* Start */
+	KEY(1, 1, KEY_LEFTSHIFT),	/* Y */
+	KEY(1, 2, KEY_LEFTALT),		/* B */
+	KEY(1, 3, KEY_LEFTCTRL),	/* A */
+
+	KEY(2, 0, KEY_RESERVED),	/* Not used */
+	KEY(2, 1, KEY_SPACE),		/* X */
+	KEY(2, 2, KEY_BACKSPACE),	/* Right Shift */
+	KEY(2, 3, KEY_ESC),		/* Select */
+};
+
+static const struct matrix_keymap_data board_keymap_data = {
+	.keymap			= board_matrix_keys,
+	.keymap_size		= ARRAY_SIZE(board_matrix_keys),
+};
+
+static unsigned int board_keypad_row_gpios[] = {
+	JZ_GPIO_PORTD(19),
+	JZ_GPIO_PORTE(7),
+	JZ_GPIO_PORTE(8),
+};
+
+static unsigned int board_keypad_col_gpios[] = {
+	JZ_GPIO_PORTE(18),
+	JZ_GPIO_PORTF(10),
+	JZ_GPIO_PORTF(11),
+	JZ_GPIO_PORTF(12),
+};
+
+static struct matrix_keypad_platform_data board_keypad_platform_data = {
+	.keymap_data	= &board_keymap_data,
+	.row_gpios	= board_keypad_row_gpios,
+	.num_row_gpios	= ARRAY_SIZE(board_keypad_row_gpios),
+	.col_gpios	= board_keypad_col_gpios,
+	.num_col_gpios	= ARRAY_SIZE(board_keypad_col_gpios),
+	.active_low	= 1,
+
+	.debounce_ms		= 5,
+	.col_scan_delay_us	= 20,
+};
+
+static struct platform_device rzx50_matrix_keypad_device = {
+	.name	= "matrix-keypad",
+	.id	= -1,
+	.dev	= {
+		.platform_data = &board_keypad_platform_data,
+	},
+};
+
+/* LCD backlight */
+static struct platform_pwm_backlight_data rzx50_backlight_pdata = {
+	.pwm_id = 2,
+	.max_brightness = 255,
+	.dft_brightness = 145,
+	.pwm_period_ns = 40000, /* 25 kHz: outside human hearing range */
+};
+
+static struct platform_device rzx50_backlight_device = {
+	.name = "pwm-backlight",
+	.id = -1,
+	.dev = {
+		.platform_data = &rzx50_backlight_pdata,
+	},
+};
+
+/* All */
+static struct platform_device *jz_platform_devices[] __initdata = {
+	&jz_lcd_device,
+	&jz_udc_device,
+	//&jz_i2c_device,
+	&jz_msc0_device,
+	&jz_msc1_device,
+	&rzx50_gpio_keys_device,
+	&rzx50_matrix_keypad_device,
+	&rzx50_backlight_device,
+};
+
+static int __init rzx50_init_platform_devices(void)
+{
+	jz_msc0_device.dev.platform_data = &cetus_sd_data;
+	jz_msc1_device.dev.platform_data = &cetus_tf_data;
+
+	jz4750d_serial_device_register();
+
+	return platform_add_devices(jz_platform_devices, ARRAY_SIZE(jz_platform_devices));
+}
+
+static void __init board_gpio_setup(void)
+{
+	int i;
+
+	__gpio_as_output(GPIO_AMPEN);
+	__gpio_clear_pin(GPIO_AMPEN);
+#ifdef GPIO_CHARGE
+	__gpio_as_output(GPIO_CHARGE);
+	__gpio_set_pin(GPIO_CHARGE);
+#endif
+
+	/* Set up gpios for keys */
+	for(i = 0; i < ARRAY_SIZE(board_keypad_row_gpios); i++) {
+		__gpio_as_func0(board_keypad_row_gpios[i]);
+		__gpio_as_input(board_keypad_row_gpios[i]);
+		__gpio_enable_pull(board_keypad_row_gpios[i]);
+	  
+	}
+
+	for(i = 0; i < ARRAY_SIZE(board_keypad_col_gpios); i++) {
+		__gpio_as_func0(board_keypad_col_gpios[i]);
+		__gpio_as_input(board_keypad_col_gpios[i]);
+		__gpio_enable_pull(board_keypad_col_gpios[i]);
+	  
+	}
+}
+
+static int __init jz_board_setup(void)
+{
+	printk("Ritmix RZX50 board setup %s %s\n",__DATE__,__TIME__);
+
+	board_gpio_setup();
+
+	if (rzx50_init_platform_devices())
+		panic("Failed to initialize platform devices");
+
+	return 0;
+}
+
+arch_initcall(jz_board_setup);
---
+++ arch/mips/jz4750d/setup.c
@@ -0,0 +1,130 @@
+/*
+ * linux/arch/mips/jz4750d/common/setup.c
+ * 
+ * JZ4750D common setup routines.
+ * 
+ * Copyright (C) 2006 Ingenic Semiconductor Inc.
+ *
+ *  This program is free software; you can distribute it and/or modify it
+ *  under the terms of the GNU General Public License (Version 2) as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place - Suite 330, Boston MA 02111-1307, USA.
+ *
+ */
+#include <linux/init.h>
+#include <linux/io.h>
+#include <linux/ioport.h>
+#include <linux/kernel.h>
+
+#include <asm/cpu.h>
+#include <asm/bootinfo.h>
+#include <asm/irq.h>
+#include <asm/mipsregs.h>
+#include <asm/reboot.h>
+#include <asm/pgtable.h>
+#include <asm/time.h>
+
+#ifdef CONFIG_PC_KEYB
+#include <asm/keyboard.h>
+#endif
+
+#include <asm/mach-jz4750d/jz4750d_intc.h>
+#include <asm/mach-jz4750d/jz4750d_cpm.h>
+
+#include "reset.h"
+
+jz_clocks_t jz_clocks;
+
+static void __init sysclocks_setup(void)
+{
+	jz_clocks.cclk = __cpm_get_cclk();
+	jz_clocks.hclk = __cpm_get_hclk();
+	jz_clocks.pclk = __cpm_get_pclk();
+	jz_clocks.mclk = __cpm_get_mclk();
+	jz_clocks.h1clk = __cpm_get_h1clk();
+	jz_clocks.pixclk = __cpm_get_pixclk();
+	jz_clocks.i2sclk = __cpm_get_i2sclk();
+	jz_clocks.usbclk = __cpm_get_usbclk();
+	jz_clocks.mscclk = __cpm_get_mscclk(0);
+	jz_clocks.extalclk = __cpm_get_extalclk();
+	jz_clocks.rtcclk = __cpm_get_rtcclk();
+
+	printk("CPU clock: %dMHz, System clock: %dMHz, "
+		"Peripheral clock: %dMHz, Memory clock: %dMHz\n",
+		(jz_clocks.cclk + 500000) / 1000000,
+		(jz_clocks.hclk + 500000) / 1000000,
+		(jz_clocks.pclk + 500000) / 1000000,
+		(jz_clocks.mclk + 500000) / 1000000);
+}
+
+static void __init soc_cpm_setup(void)
+{
+	/* Start all module clocks
+	 */
+	__cpm_start_all();
+
+	/* Enable CKO to external memory */
+	__cpm_enable_cko();
+
+	/* CPU enters IDLE mode when executing 'wait' instruction */
+	__cpm_idle_mode();
+
+	/* Setup system clocks */
+	sysclocks_setup();
+}
+
+static void __init soc_harb_setup(void)
+{
+//	__harb_set_priority(0x00);  /* CIM>LCD>DMA>ETH>PCI>USB>CBB */
+//	__harb_set_priority(0x03);  /* LCD>CIM>DMA>ETH>PCI>USB>CBB */
+//	__harb_set_priority(0x0a);  /* ETH>LCD>CIM>DMA>PCI>USB>CBB */
+}
+
+static void __init soc_emc_setup(void)
+{
+}
+
+static void __init soc_dmac_setup(void)
+{
+	__dmac_enable_module(0);
+	__dmac_enable_module(1);
+}
+
+static void __init jz_soc_setup(void)
+{
+	soc_cpm_setup();
+	soc_harb_setup();
+	soc_emc_setup();
+	soc_dmac_setup();
+}
+
+void __init plat_mem_setup(void)
+{
+	/* IO/MEM resources. Which will be the addtion value in `inX' and
+	 * `outX' macros defined in asm/io.h */
+	set_io_port_base(0);
+	ioport_resource.start	= 0x00000000;
+	ioport_resource.end	= 0xffffffff;
+	iomem_resource.start	= 0x00000000;
+	iomem_resource.end	= 0xffffffff;
+
+	jz4750d_reset_init();
+
+	jz_soc_setup();
+
+	/* FIXME: Add memory size detection */
+	add_memory_region(0, 0x04000000 /* 64M */, BOOT_MEM_RAM);
+}
+
+const char *get_system_type(void)
+{
+	return "JZ4750D";
+}
